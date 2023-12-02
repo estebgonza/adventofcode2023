654e3a45334465febdfb4c0631eed7db77489712
estebgonza <esteban.gonzalez@achilio.com>
2023-12-02 10:00:47 +0100

    feat(day1): second part

--- day1/index.ts
+++ day1/index.ts
@@ -1,10 +1,41 @@
-function firstAndLastDigits(s: string): [string, string] | undefined {
-  const digits = s.match(/\d/g);
-  if (digits) {
-    const first = digits[0];
-    const second = digits.length === 1 ? first : digits[digits.length - 1];
-    return [first, second];
+const literalNumbers = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+function findFirstOrLatestTokens(
+  input: string,
+  findFirst: boolean
+): string | null {
+  const numberStrings = [
+    ...Object.keys(literalNumbers),
+    ...Object.values(literalNumbers).map((n) => n.toString()),
+  ];
+  let tokenFound: string | null = null;
+  let tokenfFoundIndex = findFirst ? input.length : -1;
+  for (const numberString of numberStrings) {
+    const pos = findFirst
+      ? input.indexOf(numberString)
+      : input.lastIndexOf(numberString);
+    const isBetter = findFirst
+      ? pos < tokenfFoundIndex
+      : pos > tokenfFoundIndex;
+    if (pos !== -1 && isBetter) {
+      tokenFound = numberString;
+      tokenfFoundIndex = pos;
+    }
   }
+  return tokenFound;
+}
+function tokenFoundToNumber(s: string): string {
+  const n = parseInt(s);
+  return (Number.isNaN(n) ? literalNumbers[s] : n).toString();
 }
 (async () => {
   let total = 0;
@@ -12,8 +43,15 @@ function firstAndLastDigits(s: string): [string, string] | undefined {
     .trim()
     .split("\n")
     .forEach((line) => {
-      const [first, second] = firstAndLastDigits(line);
-      total += parseInt(first.toString() + second.toString());
+      const first = findFirstOrLatestTokens(line, true);
+      const second = findFirstOrLatestTokens(line, false);
+      if (first && second) {
+        const firstNumber = tokenFoundToNumber(first);
+        const secondNumber = tokenFoundToNumber(second);
+        total += parseInt(`${firstNumber}${secondNumber}`);
+      }
     });
-  console.log("Part 1:", total); // 55621
+  // Part 1: 55621
+  // Part 2: 53592
+  console.log("Result: ", total);
 })();
